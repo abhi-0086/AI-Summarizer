d6bccfc97b88f029e895fdf9ec5870b64ac1913c
abhi-0086 <babaabhishek86@gmail.com>
2023-04-26 17:32:30 +0530

    created redux endpoints with rapidapi

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,14 +1,27 @@
 import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
-  const [articles, setArticle] = useState({
+  const [article, setArticle] = useState({
     url: "",
     summary: "",
   });
 
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
   const handleSubmit = async (e) => {
-    alert("Submitted");
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+
+      setArticle(newArticle);
+
+      console.log(newArticle);
+    }
   };
 
   return (
@@ -27,8 +40,8 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Enter a URL"
-            value={articles.url}
-            onChange={(e) => setArticle({ ...articles, url: e.target.value })}
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className="url_input peer"
           />
